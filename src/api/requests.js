785e29c1128722d00a5806695c35ebcc714093c1
Eder Gonzalez <ederyirnglz@gmail.com>
2018-03-26 19:57:42 -0600

    fix requests

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,17 +2,13 @@ import request from './index';
 
 
 export function getPoll(key) {
-  return request(`http://localhost:9000/questions/${key}`).then(res => res);
+  return request(`https://vue-poll.herokuapp.com/questions/${key}`).then(res => res);
 }
 
 export function sendAnswers(data) {
-  return request('http://localhost:9000/answers', 'POST', data).then(res => res);
-}
-
-export function login(data) {
-  return request('http://localhost:9000/auth', 'POST', data).then(res => res);
+  return request('https://vue-poll.herokuapp.com/answers', 'POST', data).then(res => res);
 }
 
 export function dashboard() {
-  return request('http://localhost:9000/todayAnswers').then(res => res);
+  return request('https://vue-poll.herokuapp.com/todayAnswers').then(res => res);
 }
